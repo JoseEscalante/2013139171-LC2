d884cb45024d8a4e783346ec2521bbbbbc6bb443
Jose Escalante <coti.942@gmail.com>
2017-06-10 23:31:44 -0500

    LC3 - LC4

---
+++ 2013139171-SLN/2013139171-API/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace _2013139171_API.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ 2013139171-SLN/2013139171-API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace _2013139171_API.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
